54b36ebc156ddeb9e3424b485b1700a081e81f84
Nirmean <ana_noona@live.com>
2024-01-09 16:27:28 +0000

    added code to retrieve data from local storage and display in text area

--- starter/script.js
+++ starter/script.js
@@ -18,14 +18,26 @@ function setTimetable() {
 }
 setTimetable();
 
+// dynamically create new row and columns 
 function createRow(rowHour, savedText) {
     var newRow = $("<div>").addClass("row");
     var hourCol = $("<div>").addClass("col hour").text(formatHourNumber(rowHour));
     var textCol = $("<textarea>").addClass("col text-area").text(savedText);
-    var saveBtn = $("<button>").addClass("col save-button");
-    newRow.append(hourCol, textCol, saveBtn);
+    var buttonCol = $("<button>").addClass("col save-button");
+    newRow.append(hourCol, textCol, buttonCol);
 
+    // add event listener to button column,
+    // get the text from text area and save in local storage
+    buttonCol.on("click", function() {
+        var newText = textCol.val();
+        localStorage.setItem("savedText" + rowHour, newText);
+    });
 
+    // retrieve the data from local storage + display in text area
+    var savedText = localStorage.getItem("savedText" + rowHour, newText);
+    if (savedText) {
+        textCol.val(savedText);
+    }
 
 
 }
