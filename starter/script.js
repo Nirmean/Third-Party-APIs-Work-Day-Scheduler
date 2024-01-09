06947fe1807184cfaa1f082f8eaf733426a9c6c6
Nirmean <ana_noona@live.com>
2024-01-09 16:23:10 +0000

    function to create timetable and rows

--- starter/script.js
+++ starter/script.js
@@ -10,6 +10,28 @@ setCurrentDay();
 
 // function create the timetable
 
+function setTimetable() {
+    const timetableEl = $("#timetable");
+    for (let i = 9; i <= 18; i++) {
+        timetableEl.append(createRow(i));
+    }
+}
+setTimetable();
+
+function createRow(rowHour, savedText) {
+    var newRow = $("<div>").addClass("row");
+    var hourCol = $("<div>").addClass("col hour").text(formatHourNumber(rowHour));
+    var textCol = $("<textarea>").addClass("col text-area").text(savedText);
+    var saveBtn = $("<button>").addClass("col save-button");
+    newRow.append(hourCol, textCol, saveBtn);
+
+
+
+
+}
+
+createRow();
+
 // function to change timeblock colors according to time
 
 // function to retrieve, save and display data from the local storage
